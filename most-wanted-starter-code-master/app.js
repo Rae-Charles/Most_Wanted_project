45a0ba87abd1db4c847a0cd996b551ae5a3f9d97
Rachel Harris <rachelharris@MacBook-Air.local>
2022-01-14 22:10:33 -0600

    Final syntax for prompts.

--- most-wanted-starter-code-master/app.js
+++ most-wanted-starter-code-master/app.js
@@ -183,7 +183,7 @@ function searchByGender(people){
 
 
 function searchByOccupation(people){
-  let occupation = promptFor("What is the person's occupation?", autoValid).toLowerCase();
+  let occupation = promptFor("Please enter the person's occupation.", autoValid).toLowerCase();
 
   let foundPerson = people.filter(function(potentialMatch){
     if(potentialMatch.occupation === occupation){
